a92d4a54b0c0f50f018bb6c14dc403dffadaa316
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-12-03 15:56:14 +0800

    added question3-4 and mapped it

--- frontend/src/pages/GetStarted.tsx
+++ frontend/src/pages/GetStarted.tsx
@@ -4,14 +4,11 @@ import "../styles/pages/GetStarted.css";
 import { useState } from "react";
 import questions_1 from "../utils/question1.json";
 import questions_2 from "../utils/question2.json";
+import questions_3 from "../utils/question3.json";
+import questions_4 from "../utils/question4.json";
 
 function GetStarted() {
   interface Question {
-    id: string;
-    option: string;
-  }
-
-  interface Question_2 {
     id: string;
     option: string;
   }
@@ -34,6 +31,24 @@ function GetStarted() {
     );
   };
 
+  // Add separate state for question 3
+  const [selectedQ3, setSelectedQ3] = useState<string[]>([]);
+
+  const handleChangeQ3 = (id: string) => {
+    setSelectedQ3((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
+  // Add separate state for question 3
+  const [selectedQ4, setSelectedQ4] = useState<string[]>([]);
+
+  const handleChangeQ4 = (id: string) => {
+    setSelectedQ3((prev) =>
+      prev.includes(id) ? prev.filter((item) => item !== id) : [...prev, id]
+    );
+  };
+
   return (
     <section id="Get-Started">
       <Navbar />
@@ -47,18 +62,18 @@ function GetStarted() {
         </div>
 
         {/* Question 1 Start */}
-        <div className="mt-10">
+        <div className="mt-15">
           <h2 className="h2 question">
             What kinds of stories or topics do you usually enjoy?
           </h2>
           <h4 className="h4">Check all that apply.</h4>
 
           {/* Checkbox container */}
-          <div className="checkbox-container flex flex-col md:space-x-20 md:flex-row md:flex-wrap md:gap-4 md:ml-15 xl:flex-row xl:flex-wrap p-6 gap-4 xl:gap-6 xl:ml-15">
+          <div className="checkbox-container">
             {questions_1.map((option: Question, index) => (
               <label
                 key={option.id}
-                className="checkbox flex items-center w-full md:w-1/3 xl:w-1/4"
+                className="checkbox"
               >
                 <input
                   type="checkbox"
@@ -77,18 +92,18 @@ function GetStarted() {
         {/* Question 1 End */}
 
         {/* Question 2 Start */}
-        <div className="mt-10">
+        <div className="mt-10 border-t pt-10">
           <h2 className="h2 question">
             What’s your usual intent when you read?
           </h2>
           <h4 className="h4">Check all that apply.</h4>
 
           {/* Checkbox container */}
-          <div className="checkbox-container flex flex-col md:space-x-20 md:flex-row md:flex-wrap md:gap-4 md:ml-15 xl:flex-row xl:flex-wrap p-6 gap-4 xl:gap-6 xl:ml-15">
+          <div className="checkbox-container">
             {questions_2.map((option: Question, index) => (
               <label
                 key={option.id}
-                className="checkbox flex items-center w-full md:w-1/3 xl:w-1/4"
+                className="checkbox"
               >
                 <input
                   type="checkbox"
@@ -106,6 +121,65 @@ function GetStarted() {
         </div>
         {/* Question 2 End */}
 
+        {/* Question 3 Start */}
+        <div className="mt-10 border-t pt-10">
+          <h2 className="h2 question">
+            What type of reading experience do you prefer?
+          </h2>
+          <h4 className="h4">Check all that apply.</h4>
+
+          {/* Checkbox container */}
+          <div className="checkbox-container">
+            {questions_3.map((option: Question, index) => (
+              <label
+                key={option.id}
+                className="checkbox"
+              >
+                <input
+                  type="checkbox"
+                  value={option.id}
+                  checked={selectedQ3.includes(option.id)}
+                  onChange={() => handleChangeQ3(option.id)}
+                  className="min-w-6 min-h-6 accent-(--color-red) md:w-6 md:h-6 xl:w-8 xl:h-8"
+                />
+                <span className="ml-2 md:text-xl xl:text-2xl">
+                  {option.option}
+                </span>
+              </label>
+            ))}
+          </div>
+        </div>
+        {/* Question 3 End */}
+
+        {/* Question 4 Start */}
+        <div className="mt-10 border-t pt-10">
+          <h2 className="h2 question">
+            Are there types of books you usually avoid?
+          </h2>
+          <h4 className="h4">Check all that apply.</h4>
+
+          {/* Checkbox container */}
+          <div className="checkbox-container">
+            {questions_4.map((option: Question, index) => (
+              <label
+                key={option.id}
+                className="checkbox"
+              >
+                <input
+                  type="checkbox"
+                  value={option.id}
+                  checked={selectedQ4.includes(option.id)}
+                  onChange={() => handleChangeQ4(option.id)}
+                  className="min-w-6 min-h-6 accent-(--color-red) md:w-6 md:h-6 xl:w-8 xl:h-8"
+                />
+                <span className="ml-2 md:text-xl xl:text-2xl">
+                  {option.option}
+                </span>
+              </label>
+            ))}
+          </div>
+        </div>
+        {/* Question 4 End */}
 
       </div>
     </section>
