7c4286273b906d4c6c0b7492c9ee8dd98601bc42
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-12-03 15:32:17 +0800

    fixed styling for checkboxes

--- frontend/src/pages/GetStarted.tsx
+++ frontend/src/pages/GetStarted.tsx
@@ -30,29 +30,34 @@ function GetStarted() {
           <h1 className="font-bold text-3xl md:text-4xl xl:text-5xl text-left">Get Started</h1>
         </div>
 
-        {/* Question 1 */}
+        {/* Question 1 Start */}
         <div className="mt-10">
           <h2 className="h2 question">What kinds of stories or topics do you usually enjoy?</h2>
           <h4 className="h4">Check all that apply.</h4>
 
           {/* Checkbox container */}
-          <div className="checkbox-container flex flex-col p-6 gap-4 xl:ml-15">
-            {questions_1.map((option: Question) => (
-              <label key={option.id} className="checkbox flex items-center">
+          <div className="checkbox-container flex flex-col md:space-x-20 md:flex-row md:flex-wrap md:gap-4 md:ml-15 xl:flex-row xl:flex-wrap p-6 gap-4 xl:gap-6 xl:ml-15">
+            {questions_1.map((option: Question, index) => (
+              <label
+                key={option.id}
+                className="checkbox flex items-center w-full md:w-1/3 xl:w-1/4"
+              >
                 <input
                   type="checkbox"
                   value={option.id}
                   checked={selected.includes(option.id)}
                   onChange={() => handleChange(option.id)}
-                  className="w-6 h-6 accent-(--color-red) md:w-6 md:h-6 xl:w-8 xl:h-8"
+                  className="min-w-6 min-h-6 accent-(--color-red) md:w-6 md:h-6 xl:w-8 xl:h-8"
                 />
                 <span className="ml-2 md:text-xl xl:text-2xl">{option.option}</span>
               </label>
             ))}
           </div>
         </div>
+        {/* Question 1 End */}
+
       </div>
-            
+
     </section>
   );
 }
