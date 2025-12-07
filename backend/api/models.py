1ecb3f1377476394a118cf97c14859e5ed4b30d7
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-12-07 18:34:45 +0800

    setup settings.py configs, and added models in models.py

---
+++ backend/api/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class DevicePreset(models.Model):
+    # Preset naming and creation details
+    device_id = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    # Preset configs
+    favorite_genres = models.JSONField(default=list, blank=True)
+    reading_interests = models.JSONField(default=list, blank=True)
+    reading_experience =  models.JSONField(default=list, blank=True)
+    avoid_types =  models.JSONField(default=list, blank=True)
