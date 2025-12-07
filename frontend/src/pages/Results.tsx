686ffb3ee9055fcbbfe56f5517ec1369492d27d3
itzjmbruhhh <reyesjohnmichael1029@gmail.com>
2025-12-07 17:21:58 +0800

    added initial styling for book results

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -6,10 +6,32 @@ type ResultsProps = {
 
 function Results({ onClose }: ResultsProps) {
   return (
-    <div className="container">
+    <div className="container p-5! xl:p-10! min-w-[350px] max-w-[350px]">
       <button onClick={onClose}>
-        <i className="las la-times absolute top-3 right-3 text-3xl"></i>
-        HELLOOO
+        <i className="las la-times absolute top-3 right-3 text-3xl cursor-pointer"></i>
+        <h1 className="text-md text-left font-semibold mt-10 mb-2 md:text-2xl xl:text-2xl">
+          Top picks from your photo
+        </h1>
+
+        <div className="books-card-container flex flex-row justify-between gap-6 mb-5">
+          {/* Book Card */}
+          <div className="book-card">
+            <img src="" alt="" className="book-image" />
+            <div className="book-text text-left">
+              <h3 className="book-title">To Kill a Mockingbird</h3>
+              <p className="author">Author name</p>
+            </div>
+          </div>
+
+          {/* Book Card */}
+          <div className="book-card">
+            <img src="" alt="" className="book-image" />
+            <div className="book-text text-left">
+              <h3 className="book-title">Book Title</h3>
+              <p className="author">Author name</p>
+            </div>
+          </div>
+        </div>
       </button>
     </div>
   );
--- frontend/src/pages/Upload.tsx
+++ frontend/src/pages/Upload.tsx
@@ -12,7 +12,7 @@ function Upload({ onClose, onProceed }: UploadProps) {
   return (
     <div className="container">
       <button onClick={onClose}>
-        <i className="las la-times absolute top-3 right-3 text-3xl"></i>
+        <i className="las la-times absolute top-3 right-3 text-3xl cursor-pointer"></i>
       </button>
 
       {/* Image Group */}
